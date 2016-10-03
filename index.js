203340d7a03b0581813d65cde6c23b56ebed80f8
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2016-10-03 14:34:15 +0330

    exec and runtimeExec added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const RenderKid = require('renderkid')
+const child_process = require('child_process');
 
 class CLI {
 
   constructor()
   {
+    this.initialize();
+  }
+
+  initialize() {
     this.r = new RenderKid();
+    this.stylize();
+
+    this.execCmd = child_process.exec;
+    this.spawnCmd = child_process.spawn;
+  }
+
+  stylize() {
     this.r.style({
       "title": {
         display: "block",
@@ -34,6 +46,51 @@ class CLI {
         margin: "0 3 1"
       }
     });
+  }
+
+  exec( cmdObj, callback ) {
+    const that = this;
+    const command = {
+      onerror: (error)=> {
+        that.cli.log(`<error> ${error} </error>`);
+      },
+      onsuccess: ()=> {}
+    }
+
+    if(typeof cmdObj == "string") command.command = cmdObj;
+    else Object.assign(command, cmdObj);
+
+    if(typeof callback == "function") command.onsuccess = callback;
+
+    this.execCmd(command.command, {
+      encoding: 'utf8',
+      maxBuffer: 1000*1024
+    }, (error, stdout, stderr) => {
+        if (error) { command.onerror.call(error); return; }
+        command.onsuccess.call();
+    });
+  }
+
+  runtimeExec( cmdObj, onData ) {
+    const that = this;
+    const command = {
+      onerror: (error)=> {
+        that.log(`<error> ${error} </error>`);
+      },
+      ondata: (data)=> {
+        that.log(`<message>${data}</message>`);
+      }
+    }
+    if(typeof cmdObj == "string") command.command = cmdObj;
+    else Object.assign(command, cmdObj);
+
+    if(typeof onData == "function") command.ondata = onData;
+
+    command.command = command.command.split(" ");
+
+    const runtime = this.spawnCmd(command.command[0], command.command.slice(1));
+    runtime.stdout.on('data', command.ondata);
+    runtime.stderr.on('data', command.onerror);
   }
 
   log( message ) {
